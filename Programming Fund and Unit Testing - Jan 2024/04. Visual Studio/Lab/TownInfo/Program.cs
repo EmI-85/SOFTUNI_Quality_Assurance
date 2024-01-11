446a7be53f6296fa6dc42ef503b8a181f2e55be3
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-11 15:41:28 +0200

    Create TownInfo

---
+++ Programming Fund and Unit Testing - Jan 2024/04. Visual Studio/Lab/TownInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town_name =  Console.ReadLine();
+            string country =  Console.ReadLine();
+            Console.WriteLine($"{town_name} is in {country}");
+        }
+    }
+}
