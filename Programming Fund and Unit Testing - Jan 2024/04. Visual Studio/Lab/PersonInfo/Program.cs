63ac571d1c78678fa7fc594f90a319d86df1574d
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-11 15:42:00 +0200

    Create PersonInfo

---
+++ Programming Fund and Unit Testing - Jan 2024/04. Visual Studio/Lab/PersonInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PersonInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name = Console.ReadLine();
+            string last_name = Console.ReadLine();
+            string country = Console.ReadLine();
+            string town = Console.ReadLine();
+            Console.WriteLine($"{first_name} {last_name} from {country} - {town}!");
+        }
+    }
+}
