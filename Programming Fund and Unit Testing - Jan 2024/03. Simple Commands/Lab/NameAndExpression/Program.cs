4883dcca901e99d4eac00e217532354d31bcb9df
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-11 15:38:13 +0200

    Create NameandExp

---
+++ Programming Fund and Unit Testing - Jan 2024/03. Simple Commands/Lab/NameAndExpression/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NameAndExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Emi");
+            Console.WriteLine(5 + 3);
+        }
+    }
+}
