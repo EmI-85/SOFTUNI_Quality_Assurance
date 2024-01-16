e42d480a751b72b1e023f66a20c2c210397652cc
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-16 10:29:00 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/05. Simple Commands Exercise/Problem2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Formats.Asn1;
+
+namespace P02.SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int area = side * side;
+            Console.WriteLine(area);
+        }
+    }
+}
