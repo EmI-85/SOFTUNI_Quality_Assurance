d633af95fa2d1123fbfe666287c00e0d2a17afec
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-16 10:30:18 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/05. Simple Commands Exercise/P05.TriangleP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P05.TrianglePerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int perimeter = a + b + c;
+            Console.WriteLine(perimeter);
+        }
+    }
+}
