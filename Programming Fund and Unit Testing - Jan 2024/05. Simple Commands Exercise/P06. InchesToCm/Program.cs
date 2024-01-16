2001f90fd3388fe9385f9ee53b826bcbd5999f82
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-16 10:33:17 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/05. Simple Commands Exercise/P06. InchesToCm/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P06._InchesToCentimetersConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthInInches = double.Parse(Console.ReadLine());
+            double sentimeters= lengthInInches * 2.54;
+            Console.WriteLine(sentimeters);
+         }
+    }
+}
