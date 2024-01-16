3854e9a42283f37fbd0a549be921bda931b31fc8
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-16 10:29:34 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/05. Simple Commands Exercise/P04.TrapezoidArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P04.TrapezoidArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int area = (a+b)/2 * height;
+            Console.WriteLine(area);
+        }
+    }
+}
