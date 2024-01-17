012ba8ea44bb1f39868251a20fc88ba3576e7232
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-17 13:51:15 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/06. Data Types and Variables Lab/03.CircleArea/Program.cs
@@ -0,0 +1,7 @@
+﻿double radiusCircle = double.Parse(Console.ReadLine());
+
+double area = radiusCircle * radiusCircle * Math.PI;
+double perimeter= 2 * Math.PI * radiusCircle;
+
+Console.WriteLine($"Area = {area:F2}");
+Console.WriteLine($"Perimeter = {perimeter:F2}");
