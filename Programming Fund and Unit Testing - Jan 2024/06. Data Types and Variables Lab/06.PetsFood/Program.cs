4d290f46df292e2ceaa3360406bab29d78356f8a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-17 13:51:46 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/06. Data Types and Variables Lab/06.PetsFood/Program.cs
@@ -0,0 +1,8 @@
+﻿int dogFoodCount = int.Parse(Console.ReadLine());
+int catFoodCount = int.Parse(Console.ReadLine());
+
+double sumDogFood = dogFoodCount * 2.50;
+double sumCatFood = catFoodCount * 4.00;
+double sumTotal = sumDogFood + sumCatFood;
+
+Console.WriteLine($"{sumTotal:F2} lv.");
