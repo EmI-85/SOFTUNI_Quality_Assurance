176f12cd9464f0807df8de10ca229c77c4a0182a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-17 13:52:16 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/06. Data Types and Variables Lab/07.Projects/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+string architectsName = Console.ReadLine();
+int projectsCount = int.Parse(Console.ReadLine());
+
+double projectTime = projectsCount * 3;
+
+Console.WriteLine($"The architect {architectsName} will need {projectTime} hours to complete {projectsCount} project/s.");
