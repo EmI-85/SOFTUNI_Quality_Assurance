8a116c5fa66fc8260c8e2d4b09da33015cd5d987
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-19 18:37:39 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/07. Simple Calculations Lab/Problem05/Program.cs
@@ -0,0 +1,8 @@
+﻿double depositedAmount = double.Parse(Console.ReadLine());
+int termInMonths = int.Parse(Console.ReadLine());
+double annualInterestRate = double.Parse(Console.ReadLine());
+
+double monthlyRate = annualInterestRate / 100 / 12;
+double amount = depositedAmount * (1 + monthlyRate * termInMonths);
+
+Console.WriteLine($"{amount:F2}");
