db7ac47c424b3ce684651dea154794a9b831bf0f
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-19 18:37:49 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/07. Simple Calculations Lab/Problem06/Program.cs
@@ -0,0 +1,9 @@
+﻿int pagesNumberInBook = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int pagesPerDay = int.Parse(Console.ReadLine());
+
+
+int bookReadingTime = pagesNumberInBook / pagesPerHour;
+int numberOfHoursPerDay = bookReadingTime / pagesPerDay;
+
+Console.WriteLine(numberOfHoursPerDay);
