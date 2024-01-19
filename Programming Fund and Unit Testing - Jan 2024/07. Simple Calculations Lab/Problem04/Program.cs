5e9c0e29e4c08747478e429ba43b2d2b90a4552a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-19 18:37:29 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/07. Simple Calculations Lab/Problem04/Program.cs
@@ -0,0 +1,11 @@
+﻿double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeight = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeight = double.Parse(Console.ReadLine());
+
+double bathroomArea = bathroomHeight * bathroomWidth;
+double areaPlusSurplus = bathroomArea * 1.1;
+double tileArea = tileHeight * tileWidth;
+double tilesNeeded = areaPlusSurplus / tileArea;
+
+Console.WriteLine(Math.Round(tilesNeeded)); 
