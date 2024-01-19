7dea806467b51a5eef11b00b314c14b866676061
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-19 18:38:02 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/07. Simple Calculations Lab/Problem07/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberPens = int.Parse(Console.ReadLine());
+int numberMarkers = int.Parse(Console.ReadLine());
+int litersCleaner = int.Parse(Console.ReadLine()); 
+int discountPerc = int.Parse(Console.ReadLine()); 
+
+
+double totalSum = numberPens * 5.80 + numberMarkers *  7.20 + litersCleaner * 1.20;
+double totalCost = totalSum - totalSum * discountPerc/100;
+
+Console.WriteLine(totalCost);
