05e093e98d0818f028fc7e206200976a154d6a9f
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-19 18:36:37 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/07. Simple Calculations Lab/Problem02/Program.cs
@@ -0,0 +1,13 @@
+﻿double number1 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine());
+
+double addition = number1 + number2;
+double subtraction = number1 - number2;
+double multiplication = number1 * number2;
+double division = number1 / number2;
+
+Console.WriteLine($"{number1:F2} + {number2:F2} = {addition:F2}");
+Console.WriteLine($"{number1:F2} - {number2:F2} = {subtraction:F2}");
+Console.WriteLine($"{number1:F2} * {number2:F2} = {multiplication:F2}");
+Console.WriteLine($"{number1:F2} / {number2:F2} = {division:F2}");
+
