dcadb69e05f62370e6771290df1ebfa4e791fb9a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-22 11:08:42 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/08. Simple Calculations Exercise/Problem06/Program.cs
@@ -0,0 +1,12 @@
+﻿int lengthInCm = int.Parse(Console.ReadLine());
+int widthInCm = int.Parse(Console.ReadLine());
+int heightInCm = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double aquariumVolume = lengthInCm * widthInCm * heightInCm;
+double volumeLiters = aquariumVolume * 0.001;
+double occupiedSpace = percentage / 100 * volumeLiters;
+double waterLiters = volumeLiters - occupiedSpace;
+
+
+Console.WriteLine($"{waterLiters:F2}");
