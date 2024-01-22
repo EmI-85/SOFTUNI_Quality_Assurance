f33efabb340c315084f376b7f253d6f48653faf6
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-22 11:08:08 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/08. Simple Calculations Exercise/Problem04/Program.cs
@@ -0,0 +1,12 @@
+﻿int chickenMenus = int.Parse(Console.ReadLine());
+int fishMenus = int.Parse(Console.ReadLine());
+int vegetarianMenus = int.Parse(Console.ReadLine());
+
+double menusPrice = chickenMenus * 10.35 + fishMenus * 12.40 + vegetarianMenus * 8.15;
+double dessertPrice = menusPrice * 0.20;
+double deliveryPrice = 2.50;
+
+double totalAmount = menusPrice + dessertPrice + deliveryPrice;
+
+Console.WriteLine(totalAmount);
+
