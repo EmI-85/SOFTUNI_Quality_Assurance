c015fe1270dbf10b3071cc6f24278c9bc84dc0b8
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-01-22 11:07:51 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/08. Simple Calculations Exercise/Problem03/Program.cs
@@ -0,0 +1,10 @@
+﻿int nylonSqm = int.Parse(Console.ReadLine());
+int paintLiters = int.Parse(Console.ReadLine());
+int thinnerLiters = int.Parse(Console.ReadLine());
+int workHours = int.Parse(Console.ReadLine());
+
+double materialsCost = (nylonSqm + 2) * 1.50 + (paintLiters * 1.10) * 14.50 + thinnerLiters * 5.00 + 0.40;
+double workCost = workHours * (0.30 * materialsCost);
+double totalCost = materialsCost + workCost;
+
+Console.WriteLine(totalCost);
