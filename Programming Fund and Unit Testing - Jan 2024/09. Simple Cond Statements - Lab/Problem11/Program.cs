84814c74efe41879981b7c757df40e811d76e59f
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-03 17:32:21 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/09. Simple Cond Statements - Lab/Problem11/Program.cs
@@ -0,0 +1,21 @@
+﻿string drinkName = Console.ReadLine();  
+string extra = Console.ReadLine();
+double price = 0;
+
+if (drinkName == "coffee")
+{
+    price = 1;
+}
+
+else if (drinkName == "tea")
+{
+    price = 0.60;
+}
+
+if (extra == "sugar")
+{
+    price += 0.40;
+}
+
+
+Console.WriteLine($"Final price: ${price:F2}");
