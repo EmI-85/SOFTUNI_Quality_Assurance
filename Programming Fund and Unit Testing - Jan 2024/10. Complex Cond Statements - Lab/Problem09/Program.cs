3f622f2f153f8c3606fa462a81e731fe95b55e10
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-04 17:54:57 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/10. Complex Cond Statements - Lab/Problem09/Program.cs
@@ -0,0 +1,23 @@
+﻿string ticketType = Console.ReadLine(); 
+int rows = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
+
+
+double price = 0;
+
+if(ticketType == "Premiere")
+{
+    price = 12.00;
+}
+else if (ticketType == "Normal")
+{
+    price = 7.50;
+}
+else if (ticketType == "Discount")
+{
+    price = 5.00;
+}
+
+double totalPrice = rows * columns * price;
+
+Console.WriteLine($"{totalPrice:F2}");
