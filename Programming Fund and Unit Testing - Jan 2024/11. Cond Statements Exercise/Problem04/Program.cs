f5c2f473d769dc565f02fbc8fd7224be09191114
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:48:10 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem04/Program.cs
@@ -0,0 +1,24 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+string mathOperator = Console.ReadLine();
+
+double result = 0;
+
+if(mathOperator == "+")
+{
+    result = num1 + num2;
+}
+else if (mathOperator == "-")
+{
+    result = num1 - num2;
+}
+else if (mathOperator == "*")
+{
+    result = num1 * num2;
+}
+else if (mathOperator == "/")
+{
+    result = num1 / num2;
+}
+
+Console.WriteLine($"{num1} {mathOperator} {num2} = {result:F2}");
