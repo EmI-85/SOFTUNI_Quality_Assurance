8eced674149f2186d6a4492009daf42eda00165c
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:49:04 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem07/Program.cs
@@ -0,0 +1,21 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+string status = "";
+
+if (day == "Sunday")
+{
+    status = "closed";
+}
+else if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday")
+{
+    if (hour >= 10 && hour <= 18)
+    {
+        status = "open";
+    }
+    else
+    {
+        status = "closed";
+    }
+}
+Console.WriteLine(status);
