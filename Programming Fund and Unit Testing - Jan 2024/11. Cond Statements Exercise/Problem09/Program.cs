4fab6c31606b0fb1d4948368e569e9012785f457
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:49:39 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem09/Program.cs
@@ -0,0 +1,9 @@
+﻿int time1 = int.Parse(Console.ReadLine());
+int time2 = int.Parse(Console.ReadLine());
+int time3 = int.Parse(Console.ReadLine());
+
+int totalSeconds = time1 + time2 + time3;   
+int timeMinutes = totalSeconds / 60;
+int timeSeconds = totalSeconds % 60;
+
+Console.WriteLine($"{timeMinutes}:{timeSeconds:D2}");   
