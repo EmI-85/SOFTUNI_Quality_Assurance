e37539c63d2df9db829b8bf7cecfd4570f0ae2b2
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:47:44 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem02/Program.cs
@@ -0,0 +1,19 @@
+﻿int balance = int.Parse(Console.ReadLine());
+int withdrawalAmount = int.Parse(Console.ReadLine());
+int limit = int.Parse(Console.ReadLine());
+
+if (withdrawalAmount > limit)
+{
+    Console.WriteLine("The limit was exceeded.");
+}
+else
+{
+    if (withdrawalAmount <= balance)
+    {
+        Console.WriteLine("The withdraw was successful.");
+    }
+    else
+    {
+        Console.WriteLine("Insufficient availability.");
+    }
+}
