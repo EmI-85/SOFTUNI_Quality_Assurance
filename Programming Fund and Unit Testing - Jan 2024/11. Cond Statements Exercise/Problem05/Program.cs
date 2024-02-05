352ad2053593e29199dc014b765a0efecf7ef0d4
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:48:22 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem05/Program.cs
@@ -0,0 +1,64 @@
+﻿string season = Console.ReadLine();
+string accommodationType = Console.ReadLine();  
+int daysCount = int.Parse(Console.ReadLine());
+
+double pricePernight = 0;
+double discount = 0;
+
+
+if(season == "Spring")
+{
+    discount = 1 - 0.20;
+
+    if(accommodationType == "Hotel")
+    {
+        pricePernight = 30;
+    }
+    else if(accommodationType == "Camping")
+    {
+        pricePernight = 10;
+    }
+}
+else if (season == "Summer")
+{
+    discount = 1 - 0.00;
+
+    if (accommodationType == "Hotel")
+    {
+        pricePernight = 50;
+    }
+    else if (accommodationType == "Camping")
+    {
+        pricePernight = 30;
+    }
+}
+else if (season == "Autumn")
+{
+    discount = 1 - 0.30;
+
+    if (accommodationType == "Hotel")
+    {
+        pricePernight = 20;
+    }
+    else if (accommodationType == "Camping")
+    {
+        pricePernight = 15;
+    }
+}
+else if (season == "Winter")
+{
+    discount = 1 - 0.10;
+
+    if (accommodationType == "Hotel")
+    {
+        pricePernight = 40;
+    }
+    else if (accommodationType == "Camping")
+    {
+        pricePernight = 10;
+    }
+}
+
+double totalCost = pricePernight * daysCount * discount;
+
+Console.WriteLine($"{totalCost:F2}");
