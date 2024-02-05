a1190cd9180103baed61fba7529c3235f8eec85d
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-05 15:49:18 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/11. Cond Statements Exercise/Problem08/Program.cs
@@ -0,0 +1,15 @@
+﻿string productName = Console.ReadLine();
+
+
+if(productName == "banana" ||productName == "apple" ||productName == "kiwi" ||productName == "cherry" ||productName == "lemon")
+{
+    Console.WriteLine("fruit");
+}
+else if (productName == "cucumber" || productName == "pepper" || productName == "carrot")
+{
+    Console.WriteLine("vegetable");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
