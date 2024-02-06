ca34c4b0f1f2108ce144e38e0a6ef8a8a0b32b38
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-06 14:08:17 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/12. For Loop/Problem03/Program.cs
@@ -0,0 +1,11 @@
+﻿int countNum = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 1; i <= countNum; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+}
+
+Console.WriteLine(sum); 
