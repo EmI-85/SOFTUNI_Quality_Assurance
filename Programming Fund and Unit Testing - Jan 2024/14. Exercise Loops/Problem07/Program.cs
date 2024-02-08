33693e51fa2635642ab6771e31cd95cb066e7ac6
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:19:15 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem07/Program.cs
@@ -0,0 +1,16 @@
+﻿int stopNum = int.Parse(Console.ReadLine());
+
+int prevNum = 0;
+
+while (true)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+ 
+    if (currentNum == stopNum)
+    {
+        Console.WriteLine(prevNum * 1.20);
+        break;
+    }
+    prevNum = currentNum;
+}
+
