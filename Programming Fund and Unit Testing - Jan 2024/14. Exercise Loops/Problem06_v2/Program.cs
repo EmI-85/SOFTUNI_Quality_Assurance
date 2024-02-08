3dfc67b775aa4d596407a180a127480b15a8b213
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:18:56 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem06_v2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = int.Parse(Console.ReadLine());
+int startNumber = number;
+
+while (startNumber > 0)
+{
+    int lastDigit = startNumber % 10;
+    startNumber = startNumber / 10;
+    if (number % lastDigit != 0)
+    {
+        Console.WriteLine($"{number} is not special");
+        break;
+    }
+    if (startNumber == 0)
+    {
+        Console.WriteLine($"{number} is special");
+        break;
+    }
+
+}
