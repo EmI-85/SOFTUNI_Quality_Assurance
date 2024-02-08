fe9cc0e888316ae6fe7ae496553e2e4bbc93791e
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:18:34 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem04/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+   char vowel = char.Parse(Console.ReadLine());  
+
+    if(vowel=='a')
+    {
+        sum += 1;
+    }
+    else if(vowel == 'e')
+    {
+        sum += 2;
+    }
+    else if (vowel == 'i')
+    {
+        sum += 3;
+    }
+    else if (vowel == 'o')
+    {
+        sum += 4;
+    }
+    else if (vowel == 'u')
+    {
+        sum += 5;
+    }
+}
+
+Console.WriteLine(sum);
