dbccd1cf5e197ef5a8a9ed2c5188ecad6f2cba8c
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:17:56 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem01/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+int multiplicationResult = 1;
+
+for(int i = 0; i < power; i++)
+{
+    multiplicationResult = multiplicationResult * number;
+}
+
+Console.WriteLine(multiplicationResult);
