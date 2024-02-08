e80048f067d2c627219c64c06b8f0a96dcd89f69
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:19:27 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem08/Program.cs
@@ -0,0 +1,21 @@
+﻿string command = Console.ReadLine();
+
+double accountBalance = 0;
+
+while(command != "End")
+{
+    double money = double.Parse(command); 
+    if (money >= 0)
+    {
+        accountBalance += money;
+        Console.WriteLine($"Increase: {money:F2}");
+    }
+    else if (money < 0)
+    {
+        accountBalance -= Math.Abs(money);
+        Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"Balance: {accountBalance:F2}");
