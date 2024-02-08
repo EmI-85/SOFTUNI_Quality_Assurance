7ddd7db7595d40010afde99c3b73d6b958526ae2
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-08 15:19:06 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/14. Exercise Loops/Problem06v2/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int tempNum = number;
+bool isSpecialNum = true;
+
+while (tempNum > 0)
+{
+    int currentLastDigit = tempNum % 10;
+    
+    if (tempNum % currentLastDigit != 0)
+    {
+        isSpecialNum = false;
+        break;
+    }
+    tempNum = tempNum / 10;
+
+    if (isSpecialNum)
+    {
+        Console.WriteLine($"{number} is special");
+    }
+    else
+    {
+        Console.WriteLine($"{number} is not special");
+    }
+}
+
