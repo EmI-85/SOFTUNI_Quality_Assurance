b788d601877f48d84042f61aaeaf901b125c0d2a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-12 10:19:34 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/15. Nested Loops/Problem07/Program.cs
@@ -0,0 +1,24 @@
+﻿string input = Console.ReadLine();
+
+
+while(input != "End")
+{
+    int number = int.Parse(input);
+    int sumDigits = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sumDigits += digit;
+        number /= 10;
+    }
+
+    Console.WriteLine($"Sum of digits = {sumDigits}");
+
+    input = Console.ReadLine();
+}
+
+if(input == "End")
+{
+    Console.WriteLine("Goodbye");
+}
