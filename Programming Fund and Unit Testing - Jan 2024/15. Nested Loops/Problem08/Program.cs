5e082119eb89142302d2af6df0e05f9e669af671
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-12 10:19:45 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/15. Nested Loops/Problem08/Program.cs
@@ -0,0 +1,21 @@
+﻿int startNum = int.Parse(Console.ReadLine());
+int endNum = int.Parse(Console.ReadLine());
+
+for  (int number = startNum; number <= endNum; number++)
+{
+    bool isPrime = true;
+
+    for(int i = 2; i < number; i++)
+    {
+        if(number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        Console.Write(number + " ");
+    }
+}
