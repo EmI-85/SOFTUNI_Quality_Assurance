616ee2006396a2100957b2d8bbf205a349e8eeb3
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-13 10:38:36 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/16. Methods/Problem05/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+int num = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(RepeatText(input, num));
+
+static string RepeatText(string text, int count)
+{
+    string newText = text;
+
+    for(int i = 1; i < count; i++)
+    {
+        newText += text;
+    }
+    return newText;
+}
