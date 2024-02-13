4e75535b216a2d743233fc44cb5ebe0058539cf9
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-13 10:38:47 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/16. Methods/Problem06/Program.cs
@@ -0,0 +1,15 @@
+﻿int baseNum = int.Parse(Console.ReadLine());
+int powNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PowerResult(baseNum, powNum));
+
+static int PowerResult(int baseNumber, int powerNumber)
+{
+    int result = baseNumber; 
+    for(int i = 2; i <= powerNumber; i++)
+    {
+        result *= baseNumber;
+    }
+
+    return result;
+}
