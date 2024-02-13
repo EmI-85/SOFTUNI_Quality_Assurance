0fefbb62208c68a2b74378a8e50daebf8d5d86f5
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-13 10:37:53 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/16. Methods/Problem01/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+PrintNumber(n);
+
+static void PrintNumber(int number)
+{
+    if(number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+
+}
