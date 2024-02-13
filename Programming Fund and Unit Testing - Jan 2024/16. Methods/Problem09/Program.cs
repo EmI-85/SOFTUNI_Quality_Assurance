dea95b03ec39b60bee5785ee105a40c3c8283548
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-13 10:39:06 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/16. Methods/Problem09/Program.cs
@@ -0,0 +1,28 @@
+﻿string product = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+double finalPrice = GetFinalPrice(product, quantity);
+Console.WriteLine($"{finalPrice:F2}");
+
+static double GetFinalPrice(string p, int q)
+{
+    double itemPrice = 0;
+
+    if(p == "coffee")
+    {
+        itemPrice = 1.50;
+    }
+    else if (p == "water")
+    {
+        itemPrice = 1.00;
+    }
+    else if (p == "coke")
+    {
+        itemPrice = 1.40;
+    }
+    else if (p == "snacks")
+    {
+        itemPrice = 2.00;
+    }
+    return itemPrice * q;
+}
