ea1bb91387484d2e45fb2650b33dc73ef45bf4bb
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-13 10:38:57 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/16. Methods/Problem07/Program.cs
@@ -0,0 +1,43 @@
+﻿string typeOfInput = Console.ReadLine();
+
+if (typeOfInput == "int")
+{
+    int n1 = int.Parse(Console.ReadLine()); 
+    int n2 = int.Parse(Console.ReadLine()); 
+    
+    Console.WriteLine(GetGreatestInt(n1, n2));
+}
+else if (typeOfInput == "char")
+{
+    char ch1 = char.Parse(Console.ReadLine());
+    char ch2 = char.Parse(Console.ReadLine());
+
+    Console.WriteLine(GetGreatestChar(ch1, ch2));
+}
+else if (typeOfInput == "string")
+{
+    string str1 = Console.ReadLine();
+    string str2 = Console.ReadLine();
+
+    Console.WriteLine(GetGreatestString(str1, str2));
+}
+
+
+
+static int GetGreatestInt(int number1, int number2)
+{
+    if (number1 > number2) return number1;
+    else return number2;
+}
+
+static char GetGreatestChar(char char1, char char2)
+{
+    if (char1 - char2 > 0) return char1;
+    else return char2;
+}
+
+static string GetGreatestString(string string1, string string2)
+{
+    if (string1.CompareTo(string2) > 0 ) return string1;
+    else return string2;
+}
