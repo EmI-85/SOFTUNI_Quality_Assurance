89ab3ff79af422a0865c1938f33fa4a0aeca6171
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-14 14:53:34 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem08/Program.cs
@@ -0,0 +1,26 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetMultiplicationSign(n1, n2, n3));
+
+static string GetMultiplicationSign(int num1, int num2, int num3)
+{
+    string sign = "";
+    int product = num1 * num2 * num3;   
+
+    if(product == 0)
+    {
+        sign = "zero";
+    }
+    else if (product < 0)
+    {
+        sign = "negative";
+    }
+    else
+    {
+        sign = "positive";
+    }
+
+    return sign;
+}
