50af49e5855f9db3e3516946f777922345715007
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-14 14:53:11 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem06/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+
+Console.WriteLine(CountOfVowels(input));
+
+static int CountOfVowels(string text)
+{
+    int counterVowels = 0;
+
+    for(int i = 0; i < text.Length; i++)
+    {
+        char ch = char.ToLower(text[i]);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        {
+            counterVowels++;
+        }
+    }
+    return counterVowels;
+}
