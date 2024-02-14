0efe57d8e29d01b20720029db9a3fa71835f3845
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-14 14:52:10 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem02/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int hundreds = 1; hundreds <= 9; hundreds++)
+{
+    for (int tens = 0; tens <= 9; tens++)
+    {
+        for (int ones = 0; ones <= 9; ones++)
+        {
+            int digitsProduct = hundreds * tens * ones;
+
+            if(digitsProduct == n)
+            {
+                Console.Write($"{hundreds}{tens}{ones} ");
+            }
+        }
+    }
+}
