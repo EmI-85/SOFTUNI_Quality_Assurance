d9dfa68b310b84f676319dafea293b8b13f8f332
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-14 14:53:01 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem04/Program.cs
@@ -0,0 +1,30 @@
+﻿char s = char.Parse(Console.ReadLine());
+char e = char.Parse(Console.ReadLine());
+char x = char.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (char i = s; i <= e; i++)
+{
+    for (char j = s; j <= e; j++)
+    {
+            for (char m = s; m <= e; m++)
+            {
+            if (i == x || j == x || m == x)
+            {
+                continue;
+            }
+            else 
+            {
+                Console.Write($"{i}{j}{m} ");
+                count++;
+            }
+            }
+    }
+}
+Console.WriteLine();
+Console.WriteLine(count);
+
+
+
+
---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem05/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 0; j <= n; j++)
+    {
+        for (int m = 0; m <= n; m++)
+        {
+            for (int k = 0; k <= n; k++)
+            {
+                int leftSum = i + j;
+                int rightSum = m + k;
+
+                if (leftSum == rightSum && rightSum == n)
+                {
+                    Console.Write($"{i}{j}{m}{k} ");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
