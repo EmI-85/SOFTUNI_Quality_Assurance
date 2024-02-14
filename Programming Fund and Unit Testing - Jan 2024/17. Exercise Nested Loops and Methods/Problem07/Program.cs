74292473cd5484e51c1c366c397bafd47863c0a8
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-14 14:53:25 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/17. Exercise Nested Loops and Methods/Problem07/Program.cs
@@ -0,0 +1,20 @@
+﻿long n1 = int.Parse(Console.ReadLine());
+long n2 = int.Parse(Console.ReadLine());
+
+double factorial1 = Factorial(n1);
+double factorial2 = Factorial(n2); 
+    
+double result = factorial1 / factorial2;
+
+
+Console.WriteLine(result);
+
+static double Factorial(double n)
+{
+    int fac = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fac *= i;
+    }
+    return fac;
+}
