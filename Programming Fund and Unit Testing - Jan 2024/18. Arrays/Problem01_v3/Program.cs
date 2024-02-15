7cbc981466af00fee3001bc858246f77aaacdd0b
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-15 13:29:27 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/18. Arrays/Problem01_v3/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] days = new string[8];
+days[0] = "Invalid day!";
+days[1] = "Monday";
+days[2] = "Tuesday";
+days[3] = "Wednesday";
+days[4] = "Thursday";
+days[5] = "Friday";
+days[6] = "Saturday";
+days[7] = "Sunday";
+
+int currentDay = int.Parse(Console.ReadLine());
+
+if (currentDay >= 1 && currentDay <= 7)
+{
+    Console.WriteLine(days[currentDay]);
+}
+else
+{
+    Console.WriteLine(days[0]);
+}
