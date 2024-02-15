8fc27a7ad01abe34df833cfc6a6b4ce874ddeaa3
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-15 13:29:38 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/18. Arrays/Problem02/Program.cs
@@ -0,0 +1,18 @@
+﻿string inputLine = Console.ReadLine();
+
+string[] strings = inputLine.Split(" ");
+int[] ints = new int[strings.Length];
+
+for (int i = 0; i < ints.Length; i++)
+{
+    ints[i] = int.Parse(strings[i]);
+}
+
+int sum = 0;
+
+for (int i = 0; i < ints.Length; i++)
+{
+    sum += ints[i];
+}
+
+Console.WriteLine(sum);
