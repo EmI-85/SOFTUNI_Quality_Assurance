dccfa1ccab8a6d877bfb6652a82ee9f999f7369c
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-15 13:30:48 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/18. Arrays/Problem06_v2/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers1 = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+int[] numbers2 = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+foreach (int num1 in numbers1)
+{
+    foreach (int num2 in numbers2)
+    {
+        if (num1 == num2)
+        {
+            Console.Write($"{num1} ");
+        }
+    }
+}
