1378ce84aa37708f248ef9e87e6d3d7131d707ef
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-18 19:03:39 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/20. Exercise Arrays and Lists/Problem01/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] inputNumbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+int outputNumbersLength = inputNumbers.Length / 2;
+
+if(inputNumbers.Length % 2 != 0)
+{
+    outputNumbersLength++;
+}
+
+int[] outputNumbers = new int [outputNumbersLength];
+
+for (int i = 0; i < outputNumbersLength; i++)
+{
+    if (inputNumbers.Length % 2 != 0 && i == outputNumbersLength - 1)
+    {
+        outputNumbers[i] = inputNumbers[i];
+        break;
+    }
+    int elementA = inputNumbers[i];
+    int elementB = inputNumbers[inputNumbers.Length - 1 - i];
+    outputNumbers[i] = elementA + elementB;
+}
+
+Console.WriteLine (string.Join (" ", outputNumbers));
