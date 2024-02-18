35970ed0e051cb250912dc0cb01ca63dd937ef02
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-18 19:03:49 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/20. Exercise Arrays and Lists/Problem02/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+
+while (input.Count > 1)
+{
+    List<int> output = new List<int>();
+
+    for (int i = 0; i < input.Count - 1; i++)
+    {
+        int sum = input[i] + input[i + 1];
+        output.Add(sum);
+    }
+    input = output;
+
+}
+Console.WriteLine(string.Join(" ", input));
