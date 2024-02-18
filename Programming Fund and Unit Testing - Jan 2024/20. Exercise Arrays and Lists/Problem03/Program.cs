bb2ee00ea26f5677bf5a7c6a7537fad2ca27978f
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-18 19:03:59 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/20. Exercise Arrays and Lists/Problem03/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] arrayOne = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+int[] arrayTwo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+List<int> mergedList = new List<int>();
+
+int longerArrayLength = Math.Max(arrayOne.Length, arrayTwo.Length);
+
+for  (int i = 0; i < longerArrayLength; i++)
+{
+    if(i < arrayOne.Length)
+
+    {
+        mergedList.Add(arrayOne[i]);
+    }
+    if(i < arrayTwo.Length)
+    {
+        mergedList.Add(arrayTwo[i]);
+    }
+}
+Console.WriteLine(string.Join(" ", mergedList));
