f37984a9b542be31eb351128b7f46d03511eb469
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-22 10:03:39 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/21. UT Methods/TestApp.UnitTests/CalculateTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class CalculateTests
+{
+    [Test]
+    public void Test_Addition()
+    {
+        // Arrange
+        Calculate calculator = new();
+
+        // Act
+        int actual = calculator.Addition(5, 2);
+
+        // Assert
+        Assert.AreEqual(7, actual, "Addition did not work properly.");
+    }
+
+    [Test]
+    public void Test_Subtraction()
+    {
+        Calculate calculate = new();
+
+        int actualResult = calculate.Subtraction(5, 2);
+
+        Assert.That(3 == actualResult, "Subtraction did not work properly.");
+    }
+}
---
+++ Programming Fund and Unit Testing - Jan 2024/21. UT Methods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string emptyString = string.Empty;
+        // Act
+        string actualResult = StringReverse.Reverse(emptyString);
+
+        // Assert
+        Assert.AreEqual(string.Empty, actualResult);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        string letter = "s";
+
+        string actualResult = StringReverse.Reverse(letter);
+        string expectedResult = "s";
+
+        Assert.AreEqual(expectedResult, actualResult, "Reversal did not work properly.");
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        string text = "stosho";
+
+        string expectedResult = "ohsots";
+        string actualResult = StringReverse.Reverse(text);
+
+        Assert.AreEqual(expectedResult, actualResult, "String is NOT reversed properly!");
+    }
+}
