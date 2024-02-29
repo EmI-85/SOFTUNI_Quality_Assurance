9223ff548186cfc422e5a68c6278a43f50a23696
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-29 12:14:34 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/27. Old Exams/Problem2_01/Program.cs
@@ -0,0 +1,36 @@
+﻿int numCount = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 0; i < numCount; i++)
+{
+    int currentnum = int.Parse(Console.ReadLine()); 
+
+    if(isPrime(currentnum))
+    {
+        sum += currentnum;
+    }
+}
+Console.WriteLine(sum);
+
+static bool isPrime(int number)
+{
+    bool isPrime = true;
+
+    if (number <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+            }
+
+        }
+    }
+    return isPrime;
+}
