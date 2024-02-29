08da3cb94fe5929626ccb400ddb061c070564e98
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-29 12:14:05 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/27. Old Exams/Problem1_01/Program.cs
@@ -0,0 +1,13 @@
+﻿int studentsCount = int.Parse(Console.ReadLine());
+
+double sumGrades = 0;
+
+for  (int i = 1; i <= studentsCount; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    sumGrades += grade;
+}
+
+double averageGrade = sumGrades/studentsCount;
+
+Console.WriteLine($"{averageGrade:F2}");
