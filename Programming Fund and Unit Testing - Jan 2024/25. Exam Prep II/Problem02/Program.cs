8d5cd78cf268b1f18ee9e324862c1b737a9bdd51
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-29 10:24:36 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/25. Exam Prep II/Problem02/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+int n = int.Parse(Console.ReadLine());
+
+int minNum = int.MaxValue;
+int maxNum = int.MinValue;
+
+
+for (int i = 0; i < n; i++)
+{
+    if (numbers[i] > maxNum)
+    {
+        maxNum = numbers[i];
+    }
+    if (numbers[i] < minNum)
+    {
+        minNum = numbers[i];
+    }
+}
+Console.WriteLine(maxNum);
+Console.WriteLine(minNum);
