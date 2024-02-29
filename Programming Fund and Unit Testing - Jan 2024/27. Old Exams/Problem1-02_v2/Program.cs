0bd41df232b98fbd36c4aafc0532692c8afef3b5
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-02-29 12:14:25 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/27. Old Exams/Problem1-02_v2/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    input.Insert(0, input[input.Count - 1]);
+    input.RemoveAt(input.Count - 1);
+}
+Console.WriteLine(string.Join(", ", input));
