8749451a27e4685e857af3043bbee59195d37bfc
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-01 16:56:38 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/26. Exam Prep III/Problem02_v2/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = input.Length - 1; i >= input.Length - n; i--)
+{
+    sum += input[i];
+}
+
+double average = sum / n;
+Console.WriteLine($"{average:F2}");
