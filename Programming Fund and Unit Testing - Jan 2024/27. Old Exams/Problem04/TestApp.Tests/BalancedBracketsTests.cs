6a6e739b1ff50c22a913fdca744712560512f90f
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-01 21:56:05 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/27. Old Exams/Problem04/TestApp.Tests/BalancedBracketsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        string[] input = new string[] { " " } ;
+
+        bool actual = BalancedBrackets.IsBalanced(input);
+        bool expected = true;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        string[] input = new string[] { "(", ")", "(", ")" };
+
+        bool actual = BalancedBrackets.IsBalanced(input);
+        bool expected = true;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { ")", "(", ")" };
+
+        bool actual = BalancedBrackets.IsBalanced(input);
+        bool expected = false;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(", ")", "(", ")", ")" };
+
+        bool actual = BalancedBrackets.IsBalanced(input);
+        bool expected = false;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(", "(", "(", "(" };
+
+        bool actual = BalancedBrackets.IsBalanced(input);
+        bool expected = false;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
