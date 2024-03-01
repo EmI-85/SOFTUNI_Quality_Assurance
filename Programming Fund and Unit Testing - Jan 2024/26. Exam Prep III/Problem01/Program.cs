864e68967c8a8f18bbc37794ea1d3040855127c8
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-01 15:13:12 +0200

    Create

---
+++ Programming Fund and Unit Testing - Jan 2024/26. Exam Prep III/Problem01/Program.cs
@@ -0,0 +1,44 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+while (num > 0)
+{
+    int currentElement = num % 10;
+
+    if (currentElement == 1)
+    {
+        Console.WriteLine("one");
+    }
+    else if (currentElement == 2)
+    {
+        Console.WriteLine("two");
+    }
+    else if (currentElement == 3)
+    {
+        Console.WriteLine("three");
+    }
+    else if (currentElement == 4)
+    {
+        Console.WriteLine("four");
+    }
+    else if (currentElement == 5)
+    {
+        Console.WriteLine("five");
+    }
+    else if (currentElement == 6)
+    {
+        Console.WriteLine("six");
+    }
+    else if (currentElement == 7)
+    {
+        Console.WriteLine("seven");
+    }
+    else if (currentElement == 8)
+    {
+        Console.WriteLine("eight");
+    }
+    else if (currentElement == 9)
+    {
+        Console.WriteLine("nine");
+    }
+    num /= 10;
+}
