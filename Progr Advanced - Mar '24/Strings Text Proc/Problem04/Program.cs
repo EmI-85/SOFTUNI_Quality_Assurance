71e1cdeb7a877f33bc6fc010a0b7dcb3caae7cca
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-07 13:59:42 +0200

    Create

---
+++ Progr Advanced - Mar '24/Strings Text Proc/Problem04/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] bannedWords = Console.ReadLine().Split(", ").ToArray();
+string text = Console.ReadLine();
+
+foreach (string word in bannedWords)
+{
+    if(text.Contains(word))
+    {
+        string replacement = new string('*', word.Length);
+        text = text.Replace(word, replacement);
+    }
+}
+
+Console.WriteLine(text);
