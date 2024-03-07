8f1ad66bf630ae417598d81a6bd1857f7867eb2c
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-07 13:59:53 +0200

    Create

---
+++ Progr Advanced - Mar '24/Strings Text Proc/Problem03/Program.cs
@@ -0,0 +1,13 @@
+﻿string word = Console.ReadLine();
+string text = Console.ReadLine();
+
+while(text.Contains(word))
+{
+    int wordIndex = text.IndexOf(word);
+    string beforeWord = text.Substring(0, wordIndex);
+    string afterWord = text.Substring (wordIndex + word.Length);
+
+    text = beforeWord + afterWord;
+}
+
+Console.WriteLine(text);
