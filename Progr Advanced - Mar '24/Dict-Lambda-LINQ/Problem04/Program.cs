f783d5a71e29f5c196878eb2f13b8784e8e33cb5
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-17 15:12:53 +0200

    Create

---
+++ Progr Advanced - Mar '24/Dict-Lambda-LINQ/Problem04/Program.cs
@@ -0,0 +1,9 @@
+﻿List<string> strings = Console.ReadLine()
+                        .Split()
+                        .Where(s => s.Length % 2 == 0)
+                        .ToList();
+
+foreach (string str in strings)
+{
+    Console.WriteLine(str);
+}
