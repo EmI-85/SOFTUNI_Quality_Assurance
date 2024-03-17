b8cd081780c658121017862cdaa670a61434bd54
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-17 15:12:27 +0200

    Create

---
+++ Progr Advanced - Mar '24/Dict-Lambda-LINQ/Problem02_v2/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+var wordsWithSynonyms = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < count; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (wordsWithSynonyms.ContainsKey(word))
+    {
+        wordsWithSynonyms[word].Add(synonym);
+
+    }
+    else
+    {
+        wordsWithSynonyms.Add(word, new() { synonym });
+    }
+}
+
+foreach (var wordPair in wordsWithSynonyms)
+{
+    string synonyms = string.Join(", ", wordPair.Value);
+    Console.WriteLine($"{wordPair.Key} - {synonyms}");
+}
