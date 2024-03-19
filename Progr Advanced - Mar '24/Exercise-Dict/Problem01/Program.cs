0d8543c05e617eb9e72a2f7c3a039f2c8f4d142b
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-19 16:11:32 +0200

    Create

---
+++ Progr Advanced - Mar '24/Exercise-Dict/Problem01/Program.cs
@@ -0,0 +1,23 @@
+﻿string input = Console.ReadLine();
+
+Dictionary<char, int> charachters = new Dictionary<char, int>();
+
+foreach(char ch in input)
+{
+    if (ch != ' ')
+    {
+        if(charachters.ContainsKey(ch))
+        {
+            charachters[ch]++;
+        }
+        else
+        {
+            charachters.Add(ch, 1);
+        }
+    }
+}
+
+foreach(var pair in charachters)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
