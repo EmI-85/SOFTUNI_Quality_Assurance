e9bf0c71019e151bef5f50f4ea141773089d3784
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-19 16:11:50 +0200

    Create

---
+++ Progr Advanced - Mar '24/Exercise-Dict/Problem03_v2/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = Console.ReadLine();
+
+var productPrice = new Dictionary<string,double>();
+var productQuantity = new Dictionary<string,int>();
+
+while (input != "buy")
+{
+    string[] inputArray = input.Split();
+    string product = inputArray[0];
+    double price = double.Parse(inputArray[1]);
+    int quantity = int.Parse(inputArray[2]);
+
+    if (!productPrice.ContainsKey(product))
+    {
+        productPrice.Add(product, price);
+        productQuantity.Add(product, quantity);
+    }
+    else
+    {
+        productPrice[product] = price;
+        productQuantity[product] += quantity;
+    }
+    input = Console.ReadLine();
+}
+foreach (var product in productPrice.Keys)
+{
+    double amount = productPrice[product] * productQuantity[product];
+
+    Console.WriteLine($"{product} -> {amount:F2}");
+}
