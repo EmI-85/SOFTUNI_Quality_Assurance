60a442e60105d69b967e1f90989a7de7fd8b7e5c
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-29 18:07:31 +0200

    Create

---
+++ Progr Advanced - Mar '24/Objects-Classes/Problem03/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Problem03
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Box> boxes = new List<Box>();
+
+            string input = Console.ReadLine();
+
+            while(input!= "end")
+            {
+                string[] inputParams = input.Split();
+
+                string serialNumber = inputParams[0];
+                string itemName = inputParams[1];
+                int itemQuantity= int.Parse(inputParams[2]);
+                double itemPrice = double.Parse(inputParams[3]);
+
+                Item item = new Item()
+                {
+                    Name = itemName,
+                    Price = itemPrice
+                };
+
+                Box box = new Box()
+                {
+                    SerialNumber = serialNumber,
+                    ItemObject = item,
+                    ItemQuantity = itemQuantity,
+                    PriceBox = itemPrice * itemQuantity
+                };
+                boxes.Add(box);
+
+                input = Console.ReadLine();
+            }
+
+            List<Box> orderedBoxes = boxes.OrderByDescending(b => b.PriceBox).ToList();
+
+            foreach(Box box in orderedBoxes)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.ItemObject.Name} - ${box.ItemObject.Price:F2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceBox:F2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }    
+        public double Price { get; set; }    
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+        public Item ItemObject { get; set; }
+        public int ItemQuantity { get; set; }
+        public double PriceBox { get; set; }
+    }
+}
