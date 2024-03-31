6020a5093711c440fc17c673d96315fe5f9b973b
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-03-31 22:57:40 +0300

    Create

---
+++ Progr Advanced - Mar '24/Exercise-Objects-Classes/Problem01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                double grade = double.Parse(input[2]);
+
+                Student student = new Student();
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Grade = grade;
+
+                students.Add(student);
+            }
+
+            foreach (Student student in students.OrderByDescending(student => student.Grade))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+
+        }
+    }
+
+
+}
+
