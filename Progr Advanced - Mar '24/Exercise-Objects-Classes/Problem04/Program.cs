aace18296c18f5569098e48bb14b60569f40963a
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-01 14:59:28 +0300

    Create

---
+++ Progr Advanced - Mar '24/Exercise-Objects-Classes/Problem04/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Problem04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();  
+
+            Dictionary <string, Trainer> trainers = new Dictionary<string, Trainer>();
+
+            while(input != "Tournament")
+            {
+                //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+                string[] inputData = input.Split();
+
+                string trainerName = inputData[0];
+                string pokemonName = inputData[1];
+                string pokemonElement = inputData[2];
+                int pokemonHealth = int.Parse(inputData[3]);
+
+               if(!trainers.ContainsKey(trainerName))
+               {
+                    var trainer = new Trainer(trainerName);
+                    trainers.Add(trainerName, trainer);
+               }
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                trainers[trainerName].Pokemons.Add(pokemon);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                foreach (var (key, trainer) in trainers)
+                {
+                    if(trainer.Pokemons.Any(x => x.Element == input))
+                    {
+                        trainer.Badges += 1;
+                    }
+                    else
+                    {
+                        foreach(var pokemon in trainer.Pokemons)
+                        {
+                            pokemon.Health -= 10;
+                        }
+
+                        trainer.Pokemons.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var (key, trainer) in trainers.OrderByDescending(x => x.Value.Badges))
+            {
+                Console.WriteLine($"{key} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ Progr Advanced - Mar '24/Exercise-Objects-Classes/Problem04/Trainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem04
+{
+    public class Trainer
+    {
+        public Trainer()
+        {
+            this.Pokemons = new List<Pokemon>();
+        }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List <Pokemon> Pokemons {  get; set; }
+
+    }
+}
