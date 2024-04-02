b13446d4a1f7145323aabd6cd0243719b604ce42
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-02 11:28:03 +0300

    Create

---
+++ Progr Advanced - Mar '24/Exception-Handling/Problem01_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem01_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            try
+            {
+                number = NotNegativeNum(number);
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+         static double NotNegativeNum(double input)
+         {
+            if(input < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+
+            return input;
+         }
+    }
+}
