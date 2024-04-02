92003107243db12b3bd450bc8ad6edefb8f8b504
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-02 11:28:13 +0300

    Create

---
+++ Progr Advanced - Mar '24/Exception-Handling/Problem02/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>();
+            int start = 1;
+            int end = 100;
+            
+            while(numbers.Count < 10)
+            {
+                try
+                {
+                    int number = ReadNumber(start, end);
+                    numbers.Add(number);
+                    start = number;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+
+            bool isParsed = int.TryParse(input, out int output);
+
+            if (!isParsed)
+            {
+                throw new FormatException("Invalid Number!");
+            }
+
+            if (output <= start || output >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - {end}!");
+            }
+
+            return output;
+        }
+    }
+}
