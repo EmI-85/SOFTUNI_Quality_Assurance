a5ef6200e26f66b216dbccc30424b08f5b4dc09e
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-21 18:48:45 +0300

    Create

---
+++ Progr Advanced - Mar '24/20. JavaScriptBasics/5.MathOperations.js
@@ -0,0 +1,27 @@
+function calculate(firstNum, secondNum, operator){
+    switch(operator){
+        case '+':
+            console.log(firstNum + secondNum);
+            break;
+                    case '+':
+            console.log(firstNum + secondNum);
+            break;
+        case '-':
+            console.log(firstNum - secondNum);
+        break;
+        case '*':
+            console.log(firstNum * secondNum);
+        break;
+        case '/':
+            console.log(firstNum / secondNum);
+        break;
+        case '%':
+            console.log(firstNum % secondNum);
+        break;
+        case '**':
+            console.log(firstNum ** secondNum);
+        break;
+    }
+}
+
+calculate(3, 5.5, '*');
---
+++ Progr Advanced - Mar '24/20. JavaScriptBasics/6.LargestNumber.js
@@ -0,0 +1,6 @@
+function printLargestNumber(n1, n2, n3){
+    let largestNum = Math.max(n1, n2, n3);
+    console.log(`The largest number is ${largestNum}.`);
+}
+
+printLargestNumber(-3, -5, -22.5);
