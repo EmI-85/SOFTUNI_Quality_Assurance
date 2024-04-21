68e408a4a92ca2eae4521cfc4ef287723ee0ad7d
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-21 18:48:29 +0300

    Create

---
+++ Progr Advanced - Mar '24/20. JavaScriptBasics/3.ExcellentGrade.js
@@ -0,0 +1,10 @@
+function PrintGradeType(grade){
+    if (grade >= 5.50){
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+
+PrintGradeType(5.50)
+PrintGradeType(4.35)
