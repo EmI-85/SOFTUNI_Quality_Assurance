03b51687c0aa9f60d94827aa37427edba4853b45
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-21 18:49:17 +0300

    Create

---
+++ Progr Advanced - Mar '24/20. JavaScriptBasics/10.NumbersFromMtoN.js
@@ -0,0 +1,7 @@
+function printMtoN(numM, numN){
+    for(let i = numM; i >= numN; i--) {
+        console.log(i);
+    }
+}
+
+printMtoN(6, 2);
---
+++ Progr Advanced - Mar '24/20. JavaScriptBasics/9.NumbersFrom1to5.js
@@ -0,0 +1,7 @@
+function print1to5(input){
+    for(let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+print1to5(1);
