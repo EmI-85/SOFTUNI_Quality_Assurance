a1881d91574833a8a436b86dc895db1aa9703478
Emiliya Kirina <emiliya.kirina@gmail.com>
2024-04-30 09:29:29 +0300

    Create

---
+++ Progr Advanced - Mar '24/22. JSFundLab/04-Substring.js
@@ -0,0 +1,8 @@
+function printSubstring(input, start, count){
+let result = input.substring(start, start + count);
+
+console.log(result);
+}
+
+printSubstring('ASentence', 1, 8);
+printSubstring('SkipWord', 4, 7);
